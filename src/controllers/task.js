98d1a1b5aad09ea8c6840e2a4a044dc08110e89f
Chigozie Mbama Mykel <92220456+mbamamykel77@users.noreply.github.com>
2023-09-08 21:30:40 +0100

    date

--- src/controllers/task.js
+++ src/controllers/task.js
@@ -3,7 +3,7 @@ const info = async (req, res) => {
     const slack_name = req.query.slack_name || "Chigozie Mbama (Mykel)";
     const track = req.query.track || "Backend";
     const current_day = new Date().toLocaleString("en-US", { weekday: "long",});
-    const utc_time = new Date().toISOString();
+    const utc_time = new Date().toISOString().split('.')[0] + 'Z';
     const github_file_url = "https://github.com/yourusername/yourrepo/blob/main/yourfile.js";
     const github_repo_url = "https://github.com/mbamamykel77/Zuri-Task-1";
 
